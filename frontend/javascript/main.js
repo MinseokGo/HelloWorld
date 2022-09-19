e145dda5bd33b25b1d0cf71b415edeb00614b43b
MinseokGO <96585636+MinseokGo@users.noreply.github.com>
2022-09-19 19:15:55 +0900

    update js

---
+++ frontend/javascript/main.js
@@ -0,0 +1,18 @@
+function clickProfile() {
+    location.href = "profile.html";   
+}
+function clickQuestion() {
+    location.href = "question.html";   
+}
+function clickUser() {
+    location.href = "user.html";   
+}
+ function clickSignin() {
+    location.href = "signin.html";   
+ }
+ function clickSignup() {
+    location.href = "signup.html";   
+}
+function clickforgot() {
+    location.href = "forgot.html";   
+}
